7b8d0ce5ac2faf1e9b7da52970c1aeb453c4e333
Peng Yu <peng.yu@gmail.com>
2025-01-25 22:39:00 -0500

    extend display years to 2030

--- app/src/Constants.js
+++ app/src/Constants.js
@@ -139,7 +139,7 @@ function fillConstants(variable, name, satellites, hasDayNight) {
 }
 
 export const MIN_YEAR_LOOKBACK = 2014;
-export const VALID_YEARS_RANGE = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
+export const VALID_YEARS_RANGE = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030];
 export const VALID_DAY_RANGE = Array.from({ length: 31 }, (v, k) => k + 1);
 export const FORWARD = "forward-change";
 export const BACKWARD = "backward-change";
